178dfe24df17553b56e91913bbecda731d3ead80
ZhangYichao <yichao0803@qq.com>
2021-02-22 23:52:37 +0800

    修改 v3 版本增加原始值输出到 LOG

--- ch06-mr-dev/src/main/java/v3/MaxTemperatureMapper.java
+++ ch06-mr-dev/src/main/java/v3/MaxTemperatureMapper.java
@@ -12,29 +12,31 @@ import v2.NcdcRecordParser;
 
 //vv MaxTemperatureMapperV3
 public class MaxTemperatureMapper
-  extends Mapper<LongWritable, Text, Text, IntWritable> {
+        extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  /*[*/enum Temperature {
-    OVER_100
-  }/*]*/
-  
-  private NcdcRecordParser parser = new NcdcRecordParser();
+    /*[*/enum Temperature {
+        OVER_100
+    }/*]*/
 
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-    
-    parser.parse(value);
-    if (parser.isValidTemperature()) {
-      int airTemperature = parser.getAirTemperature();
-      /*[*/if (airTemperature > 1000) {
-        System.err.println("Temperature over 100 degrees for input: " + value);
-        context.setStatus("Detected possibly corrupt record: see logs.");
-        context.getCounter(Temperature.OVER_100).increment(1);
-      }/*]*/
-      context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+    private NcdcRecordParser parser = new NcdcRecordParser();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        parser.parse(value);
+        if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+            System.out.println("input: " + value + ", airTemperature: " + airTemperature);
+            /*[*/
+            if (airTemperature > 1000) {
+                System.err.println("Temperature over 100 degrees for input: " + value);
+                context.setStatus("Detected possibly corrupt record: see logs.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }/*]*/
+            context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+        }
     }
-  }
 }
 //^^ MaxTemperatureMapperV3
 
